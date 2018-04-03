1795c7f6d76ee8f026c86b08db761655c8216458
Sebastian Hendowski <shendows@cisco.com>
2018-04-03 13:53:24 -0700

    use process.env.port

--- ui/server.js
+++ ui/server.js
@@ -7,6 +7,9 @@ const app = express()
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+let port = ( process.env.PORT || 3000 );
+
 app.use('/db', express.static(__dirname + '/../db'));
 
 console.log(__dirname);
@@ -16,4 +19,4 @@ app.get('/', (req, res) => {
     res.send("Up and running!")
 })
 
-app.listen(8080, () => console.log('Listening on port 8080!'))
+app.listen(port, () => logger.info(`Listening on port ${port}!`))
