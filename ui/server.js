cc3bb4f2b4df9ebf8dbd898546bc91447c3e6e44
Seb <4062679+hellos3b@users.noreply.github.com>
2018-04-03 13:40:09 -0700

    maybe now?

--- ui/server.js
+++ ui/server.js
@@ -1,25 +1,29 @@
 import express from 'express'
 
 import MeetupsDB from '../bot/MeetupsDB'
+import logger from 'winston'
 
 export default {
     start() {
         const app = express()
 
         app.get('/', (req, res) => {
+            logger.info("get /")
             res.send("Up and running!")
         })
 
         app.get('/meetups.json', (req, res) => {
+            logger.info("get /meetups.json")
             let json = MeetupsDB.getMeetups();
             res.send(json)
         });
         
         app.get('/archive.json', (req, res) => {
+            logger.info("get /archive.json")
             let json = MeetupsDB.getArchive();
             res.send(json)
         });
         
-        app.listen(8080, () => console.log('Listening on port 8080!'))
+        app.listen(80, () => console.log('Listening on port 80!'))
     }
 }
