c8370c9a3bba32b1d24818211ce48eab57895152
Sebastian Hendowski <shendows@cisco.com>
2018-04-03 17:22:21 -0700

    Fixed MEETUP > MEETUPS

--- bot/Meetup.js
+++ bot/Meetup.js
@@ -15,6 +15,8 @@ const States = {
     CANCELLED: 2
 };
 
+console.log("meetup", channels.MEETUPS);
+
 export default function({ 
     id = GUID(), 
     date, 
@@ -135,7 +137,7 @@ export default function({
         return new Promise( async function(resolve, reject) {
             logger.info("Announcing meetup");
             let response = await bot.sendMessage({
-                to: channels.MEETUP,
+                to: channels.MEETUPS,
                 message: `\`👉 ${meetup_info}\`\n`
                     +    `*Started by <@!${userID}> in <#${sourceChannelID}>* `
             });
@@ -143,20 +145,20 @@ export default function({
             info_id = response.id;
 
             let { id: msg_id } = await bot.sendMessage({
-                to: channels.MEETUP,
+                to: channels.MEETUPS,
                 message: `Going to **${info}**? \`check = Yes \:thinking: = Maybe\``
             });
 
             rsvp_id = msg_id;
 
             await bot.addReaction({
-                channelID: channels.MEETUP,
+                channelID: channels.MEETUPS,
                 messageID: msg_id,
                 reaction: "☑"
             });
                 
             await bot.addReaction({
-                channelID: channels.MEETUP,
+                channelID: channels.MEETUPS,
                 messageID: msg_id,
                 reaction: "🤔"
             });
@@ -170,7 +172,7 @@ export default function({
             logger.info("Confirming meetup");
             await bot.sendMessage({
                 to: sourceChannelID,
-                message: `Meetup added: \`${meetup_info}\` Find it in <#${channels.MEETUP}>!`
+                message: `Meetup added: \`${meetup_info}\` Find it in <#${channels.MEETUPS}>!`
             });
 
             resolve();
@@ -179,13 +181,13 @@ export default function({
 
     this.getReactions = async function(bot) {
         let maybe = await bot.getReaction({
-            channelID: channels.MEETUP,
+            channelID: channels.MEETUPS,
             messageID: rsvp_id,
             reaction: "🤔"
         });
 
         let yes = await bot.getReaction({
-            channelID: channels.MEETUP,
+            channelID: channels.MEETUPS,
             messageID: rsvp_id,
             reaction: "☑"
         });
@@ -203,14 +205,14 @@ export default function({
     this.finish = async function(bot) {
         state = States.FINISHED;
         await bot.deleteMessage({
-            channelID: channels.MEETUP,
+            channelID: channels.MEETUPS,
             messageID: rsvp_id
         });
 
         let rsvp_list = `(y: ${reactions.yes.length} m: ${reactions.maybe.length})`;
         
         await bot.editMessage({
-            channelID: channels.MEETUP,
+            channelID: channels.MEETUPS,
             messageID: info_id,
             message: `\`✅ ${meetup_info} ${rsvp_list}\`\n`
         });
@@ -218,14 +220,14 @@ export default function({
 
     this.editInfo = async function(bot) {
         await bot.editMessage({
-            channelID: channels.MEETUP,
+            channelID: channels.MEETUPS,
             messageID: info_id,
             message: `\`${meetup_info}\`\n`
                 +    `*Started by <@!${userID}> in <#${sourceChannelID}>* `
         });
 
         await bot.editMessage({
-            channelID: channels.MEETUP,
+            channelID: channels.MEETUPS,
             messageID: rsvp_id,
             message: `Going to **${info}**? \`check = Yes \:thinking: = Maybe\``
         });
@@ -234,12 +236,12 @@ export default function({
     this.cancel = async function(bot) {
         state = States.CANCELLED;
         await bot.deleteMessage({
-            channelID: channels.MEETUP,
+            channelID: channels.MEETUPS,
             messageID: rsvp_id
         });
 
         await bot.editMessage({
-            channelID: channels.MEETUP,
+            channelID: channels.MEETUPS,
             messageID: info_id,
             message: `\`❌ (canceled) ${meetup_info}\`\n`
         });
