e5a71b4097f67cc1e4f728b7736b7730d0d72493
Seb <4062679+hellos3b@users.noreply.github.com>
2018-07-10 15:01:28 -0700

    added more ban reasons

--- bot/banreasons.js
+++ bot/banreasons.js
@@ -7,7 +7,6 @@ const list = [
     "Has a voat account",
     "Walks too slow",
     "Owns a fedora (rule 3 no fedoras)",
-    "Wore white",
     "Drives a bolt (rule 1 #bolt is gone)",
     "Saying pineapple on a pizza is okay",
     "Producing too much salt",
@@ -23,12 +22,22 @@ const list = [
     "Went AFK in boombot",
     "Didn't wave back to PJ (rule 6 be nice)",
     "Posting your nudes in #general",
-    "Smuggling Kinder Surprise Eggs across the border",
+    "Smuggling Kinder Surprise Eggs across the border in 2005",
     "For being Canadian",
     "Because fuck you that's why",
     "😂😂😂",
     "🖕🖕🔥😤🔥🖕🖕",
-    "Disliking a dog photo in #aww (rule 7 no bad doggers)"
+    "Disliking a dog photo in #aww (rule 7 no bad doggers)",
+    "Wearing socks with sandals",
+    "ict3 (rule 8)",
+    "Not turning volume up when Toto's Africa came on",
+    "Eating unfrosted pop tarts",
+    "Joining drunk VC without any alcohol (rule 9 get hydrated)",
+    "Using white theme on discord",
+    "Inviting people to eat leftover Lasagna but not actually having any",
+    "Giving your SO a potato as a gift (rule 10 don't be shitty)",
+    "Didn't come through",
+    "Ghosting Stacy"
 ];
 
 export default {
