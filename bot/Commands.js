69f5d947581866d91b0397e8ccb7e3c3e44670b4
Sebastian Hendowski <shendows@cisco.com>
2018-07-17 19:22:51 -0700

    lol i suck

--- bot/Commands.js
+++ bot/Commands.js
@@ -245,7 +245,7 @@ export default {
     },
 
     "!strava": async function({bot, message, channelID, userID, user}) {
-        if (channelID !== channels.ADMIN || channelID !== channels.RUN) {
+        if (channelID !== channels.ADMIN && channelID !== channels.RUN) {
             await bot.sendMessage({
                 to: channelID,
                 message: "This command only works in the 5K channel"
