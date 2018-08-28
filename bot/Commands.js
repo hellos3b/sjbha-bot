41ea1f9be51b4a0918daca89714028fbef775b80
CheeseJaguar <cheesejaguar@gmail.com>
2018-08-27 18:56:18 -0700

    fixed that for you sebby

--- bot/Commands.js
+++ bot/Commands.js
@@ -64,7 +64,7 @@ export default {
     "!ping"({bot, channelID}) {
         bot.sendMessage({
             to: channelID,
-            message: "👋"
+            message: "PONG!"
         });
     },
 
