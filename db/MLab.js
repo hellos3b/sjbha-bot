dbd60d6286bc5c396ee58a01ae6e3469fda9d60b
Sebastian Hendowski <shendows@cisco.com>
2018-09-02 19:01:55 -0700

    debug mlab

--- db/MLab.js
+++ db/MLab.js
@@ -7,6 +7,7 @@ let db;
 
 export default {
     connect() {
+        console.log("Connect", process.env.MLAB_USERNAME);
         mongoose.connect(`mongodb://${process.env.MLAB_USERNAME}:${process.env.MLAB_PASSWORD}@ds149059.mlab.com:49059/bored-humans`)
         db = mongoose.connection;
         db.on('error', console.error.bind(console, 'connection error:'));
