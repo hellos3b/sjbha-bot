efbc83b503b192e3851a5a110475ee9e27a57b29
Sebastian Hendowski <shendows@cisco.com>
2018-09-12 13:55:39 -0700

    eh

--- plugins/Outbreak/index.js
+++ plugins/Outbreak/index.js
@@ -47,10 +47,10 @@ export default function(bastion, opt={}) {
                         `At 0:00 we first spotted signs of infection in ${context.user}` :
                         `At 0:01 an immunity was discovered by ${context.user}`;
 
-                    return `CAPTIANS LOG\n` +
+                    return bastion.helpers.code(`CAPTAINS LOG\n` +
                         `${tagline}\n\n` +
                         `MESSAGE:\n` +
-                        `     ${msg}`
+                        `     ${msg}`)
                 }
                 let result = `${context.user}\nSTATUS: ${user.infection}\n` +
                 `${hourstamp} hours after outbreak\n`
@@ -88,11 +88,33 @@ export default function(bastion, opt={}) {
 
                 if (!user) return "You haven't been tagged with anything"
 
+                const startDate = new Date("2018-09-04T18:39:00.000Z")
                 const userList = list.map( n => {
-                    return n.user
+                    const msdiff = new Date(n.timestamp).getTime() - startDate.getTime()
+                    const diff = this.msToHMS(msdiff)
+                    const hourstamp = this.pad(diff.hours) + ":" + this.pad(diff.minutes)
+                    return bastion.bot.fixMessage(`[${hourstamp}]n.user`)
                 }).join(", ")
                 const verb = (user.infection === "infected") ? "infection": "immunity"
                 return bastion.helpers.code(`${context.user} spread ${verb} to ${list.length} other users:\n${userList}`)
+            },
+
+            methods: {
+                msToHMS( ms ) {
+                    // 1- Convert to seconds:
+                    var seconds = ms / 1000;
+                    // 2- Extract hours:
+                    var hours = parseInt( seconds / 3600 ); // 3,600 seconds in 1 hour
+                    seconds = Math.floor(seconds % 3600); // seconds remaining after extracting hours
+                    // 3- Extract minutes:
+                    var minutes = parseInt( seconds / 60 ); // 60 seconds in 1 minute
+                    // 4- Keep only seconds not extracted to minutes:
+                    seconds = Math.floor(seconds % 60);
+                    return { hours, minutes, seconds };
+                },
+                pad(v) {
+                    return (v < 10) ? `0${v}` : v;
+                }
             }
         }
 
