c9ea93eb94f57874ddb864d8de3a72d15bb96e45
Sebastian Hendowski <shendows@cisco.com>
2018-09-12 11:12:27 -0700

    break -> continue

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const channels = {
     "strava": "450913008323919872",
     "boombot": "432766496700235776",
     "stocks": "363123179696422916",
-    "announcement": "430878436027006978",
-    "compact": "464561717821702144"
+    "announcement": (process.env.NODE_ENV === 'production' ) ? "430878436027006978" : "487730873278398475",
+    "compact": (process.env.NODE_ENV === 'production') ? "464561717821702144" : "488850092045107202"
 }
 
 const bastion = Bastion({
@@ -21,10 +21,6 @@ const bastion = Bastion({
     serverId: "358442034790400000",
     prefix: process.env.NODE_ENV === "production" ? "!" : "_"
 })
-
-if (process.env.NODE_ENV === "admin") {
-    bastion.channels.restrict( channels.admin )
-}
     
 // Load modules
 bastion.use(botModules(bastion), { ignore: [channels.boombot] })
