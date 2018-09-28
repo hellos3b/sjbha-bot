37ba6761d745005d214f25ff9d0cbe8c66656f1d
Seb <4062679+hellos3b@users.noreply.github.com>
2018-09-28 15:33:36 -0700

    added vote command

--- plugins/Upvotes/index.js
+++ plugins/Upvotes/index.js
@@ -7,39 +7,38 @@ import deepmerge from 'deepmerge'
 import chalk from 'chalk'
 
 const baseConfig = {
+    command: 'vote',
     upvote: ':upvote:482642169757302784',
-    downvote: ':downvote:482642189437108225',
-    phrases: [
-        "what do you think of",
-        "should i"
-    ]
+    downvote: ':downvote:482642189437108225'
 }
 
 export default function(bastion, opt={}) {
     const config = deepmerge(baseConfig, opt)
 
-    bastion.on('message', (context, evt) => {
-        for (var i = 0; i < config.phrases.length; i++) {
-            if (context.message.toLowerCase().startsWith(config.phrases[i])) {
-                tagMessage(context.channelID, context.evt.d.id)
-                return
+    return [
+        {
+            command: config.command,
+
+            validate(context, tag) {
+                console.log("validate")
+                if (context.message.split(" ").length === 1) return "Vote for what"
+            },
+
+            resolve: async function(context, tag) { 
+                const channelID = context.channelID
+                const msgId = context.evt.d.id
+                await bastion.bot.addReaction({
+                    channelID: channelID,
+                    messageID: msgId,
+                    reaction: config.upvote
+                })
+        
+                await bastion.bot.addReaction({
+                    channelID: channelID,
+                    messageID: msgId,
+                    reaction: config.downvote
+                })
             }
         }
-    })
-
-    async function tagMessage(channelID, msgId) {
-        await bastion.bot.addReaction({
-            channelID: channelID,
-            messageID: msgId,
-            reaction: config.upvote
-        });
-
-        await bastion.bot.addReaction({
-            channelID: channelID,
-            messageID: msgId,
-            reaction: config.downvote
-        });
-    }
-
-    return []
+    ]
 }
