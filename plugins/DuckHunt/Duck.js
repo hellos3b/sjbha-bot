d3222d99f85e0f1194adb3bf30b6d75aff0da27c
Sebastian Hendowski <shendows@cisco.com>
2018-10-31 13:15:46 -0700

    added flair to duckhunt

--- plugins/DuckHunt/Duck.js
+++ plugins/DuckHunt/Duck.js
@@ -1,19 +1,49 @@
-
 let ducks = {}
+
+const MISS_TIMER = 1000 * 5
+
 
 export default {
 
     create(channelID, msgId) {
-        ducks[channelID] = msgId
+        ducks[channelID] = {
+            msgId,
+            active: true,
+            misses: [],
+            timestamp: new Date()
+        }
     },
 
-    bang(channelID) {
+    bang(bastion, channelID, userID, user) {
         if (!ducks[channelID]) return null
 
-        const msgId = ducks[channelID]
+        let ts = new Date()
+        const duck = ducks[channelID]
 
-        ducks[channelID] = null
+        if (duck.active) {
+            duck.active = false
+            duck.shotTimestamp = ts
+            duck.shotBy = { userID, user }
+            duck.shotTime = ts.getTime() - duck.timestamp.getTime()
 
-        return msgId
+            setTimeout(() => {
+                const q = new bastion.Queries("DuckhuntShot")
+                q.create({
+                    shotBy: duck.shotBy,
+                    misses: duck.misses,
+                    channelID: channelID,
+                    timestamp: duck.shotTimestamp
+                })
+                ducks[channelID] = null
+            }, MISS_TIMER) 
+
+            return Object.assign({}, duck, {
+                newShot: true
+            })
+        } else {
+            duck.misses.push({ user, userID })
+        }
+
+        return duck
     }
 }
