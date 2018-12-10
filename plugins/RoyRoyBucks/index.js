dbdb9164fdd62bd7a50881629f2f567550bd3cf0
Sebastian Hendowski <sebhendowski@gmail.com>
2018-12-09 16:05:10 -0800

    can now give royroybucks

--- plugins/RoyRoyBucks/index.js
+++ plugins/RoyRoyBucks/index.js
@@ -37,7 +37,41 @@ export default function(bastion, opt={}) {
                     await q.create(user)
                 }
 
+                const [cmd, ...args] = message.split(" ")
+                console.log(cmd)
+                if (cmd === 'give') return this.route("give")
+
                 return `**Bank**: ${user.bucks} royroybucks`
+            }
+        },
+
+        {
+            action: `${config.command}:give`,
+
+            resolve: async function(context, message) {
+                let user = await q.findOne({ userID: context.userID})
+                const [cmd, amount, targetMention] = message.split(" ")
+
+                const bucks = parseInt(amount)
+                if (isNaN(bucks)) return 'Not a valid amount. `!royroybucks give 100 @user`'
+                if (bucks < 1) return 'Needs to be at least 1 royroybuck'
+                if (bucks > user.bucks) return `You don't have that many royroybucks`
+
+                if (!targetMention) return `Need to specify who to give money to. \`!royroybucks give 100 @user\``
+                const target = bastion.helpers.mentionID(targetMention)
+                const targetName = bastion.bot.fixMessage(targetMention)
+
+                let reciever = await q.findOne({ userID: target})
+
+                if (!reciever) return `${targetName} hasn't opened up a bank account yet`
+
+                user.bucks -= bucks
+                reciever.bucks += bucks
+
+                await q.update({userID: user.userID}, user)
+                await q.update({userID: reciever.userID}, reciever)
+
+                return `👌 You gave ${targetName} ${bucks} royroybucks`
             }
         }
 
