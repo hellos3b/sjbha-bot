0ec8234c9bb3f0c791b2a908feeb34465936d33f
Seb <4062679+hellos3b@users.noreply.github.com>
2019-01-07 14:20:26 -0800

    remove boombot

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const bastion = Bastion({
 })
     
 // Load modules
-bastion.use(botModules(bastion), { ignore: [channels.boombot] })
+// bastion.use(botModules(bastion), { ignore: [channels.boombot] })
 bastion.use(Boombot(bastion, {
     restrict: [channels.boombot, channels.admin]
 }))
