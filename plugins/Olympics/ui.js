4ac53da1481d39df61310d7864edae9c34717ba4
Seb <4062679+hellos3b@users.noreply.github.com>
2019-06-19 13:42:49 -0700

    fix mobile scaling

--- plugins/Olympics/ui.js
+++ plugins/Olympics/ui.js
@@ -10,44 +10,9 @@ export default (bastion, config) => {
 
       let template =  requireText('./template.html', require)
 
-      // let teamsData = [
-      //   {
-      //     name: "NBD",
-      //     players: "Seb / Jenn",
-      //     primary: "#3c78d8",
-      //     secondary: "#ffd966",
-      //     flag: 1
-      //   },
-      //   {
-      //     name: "Hi",
-      //     players: "Cheeze / knockknock",
-      //     primary: "#ff00ff",
-      //     secondary: "#00ff00",
-      //     flag: 8
-      //   },
-      //   {
-      //     name: "Chinoy",
-      //     players: "Marian / Arthur",
-      //     primary: "#ffff00",
-      //     secondary: "#ff0000",
-      //     flag: 6
-      //   }
-      // ]
-
-      // const teams = new Array(8).fill({ empty: true }).map( (n, i) => {
-      //   if (teamsData[i]) {
-      //     let t = teamsData[i]
-      //     t.flagHTML = createFlag(t.flag, t.primary, t.secondary)
-      //     return t
-      //   } else {
-      //     return n
-      //   }
-      // })
-
       const q = new bastion.Queries('Olympics')
       q.getAll()
         .then(teamsData => {
-          console.log("teamsData", teamsData)
           const teams = new Array(8).fill({ empty: true }).map( (n, i) => {
             if (teamsData[i]) {
               let t = teamsData[i]
@@ -62,15 +27,6 @@ export default (bastion, config) => {
           res.send(view)
         })
     })
-    // const q = new bastion.Queries('Outbreak')
-
-    // router.get('/', (req, res) => {
-    //     q.getAll()
-    //         .then( infections => {
-    //             let view = ui({infections})
-    //             res.send(view)
-    //         });
-    // })
-
+    
     return router
 }
