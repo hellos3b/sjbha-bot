bbde276d464a5c9563b9797bf4f482e6829f3b24
Seb <4062679+hellos3b@users.noreply.github.com>
2019-06-19 18:03:33 -0700

    fix urban dic

--- plugins/UrbanDictionary/index.js
+++ plugins/UrbanDictionary/index.js
@@ -17,14 +17,20 @@ export default function(bastion, opt={}) {
            command: `define`, 
 
            // Core of the command
-           resolve: async function(context, word) {
+           resolve: async function(context) {
+             console.log("context", context.message)
+              const [cmd, ...words] = context.message.split(" ")
+              const word = words.join(' ')
+
+              if (!word) return;
+
               const {data} = await Axios.get(`http://api.urbandictionary.com/v0/define?term=${word}`)
               const first = data.list[0]
               console.log("response?", first)
               if (!first) {
                 return `No definitions for **${word}**`
               }
-              
+
               return `**${word.toUpperCase()}:**\n\n`
                 + `${first.definition}`
                 + `\n\n`
