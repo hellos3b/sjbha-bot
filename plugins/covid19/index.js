33af010e964637333146001cf6bbfe67f23f64bc
Seb <4062679+hellos3b@users.noreply.github.com>
2020-04-13 17:06:24 -0700

    :art: Simplify kens code

--- plugins/covid19/index.js
+++ plugins/covid19/index.js
@@ -1,76 +1,63 @@
 /**
  *  Use this as the base template for a new command
- * 
+ *
  */
 
-import deepmerge from 'deepmerge';
 import Axios from "axios";
+import deepmerge from "deepmerge";
 
-
-
-const baseConfig = {
-    command: "covid19"
+function capitalizeWords(str) {
+  return str.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
 }
 
-export default function(bastion, opt={}) {
-    const config = deepmerge(baseConfig, opt)
-    const cmd = bastion.command(config.command)
-	
-    return [
-        {
-            // Command to start it
-            command: config.command, 
-            options: bastion.parsers.args(["country"]),
-			
-            // Core of the command
-            resolve: async function(context, country, name) {
-				
-				function isEmpty(str) {
-					return (!str || 0 === str.length);
-				}
-				
-				function capitalize_Words(str)
-				{
-				 return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-				}
-				
-				const url = "https://pomber.github.io/covid19/timeseries.json"
-				const jsonObj = await Axios.get(url).then(r => r.data)	
+async function getCountryStats(country) {
+  const {data} = await Axios.get('https://pomber.github.io/covid19/timeseries.json')
+  const countryData = data[country]
 
-				const countries = context.message.split(" ")
-				const countJ = countries.slice(1).join(' ')	
-				const upper = capitalize_Words(countJ);
-				
-				if (isEmpty(country)) {
-					
-					const jsonObjUS = jsonObj['US'];
-					
-					const latest = jsonObjUS[jsonObjUS.length-1];
-					const yest = jsonObjUS[jsonObjUS.length-2];
-					
-					const cdiff = latest.confirmed - yest.confirmed
-					const ddiff = latest.deaths - yest.deaths
-					const rdiff = latest.recovered - yest.recovered
-					
-					return "Currently in the US there are **"+latest.confirmed+"** *"+"(+"+cdiff+")*"+" cases of COVID19, with **"+latest.deaths+"** *(+"+ddiff+")*"+" deaths, and **"+latest.recovered+"** *(+"+rdiff+")*"+" recoveries as of **"+latest.date+"**."
-				
-				} else if (jsonObj.hasOwnProperty(upper)) {
+  if (!countryData) return null;
 
-					const uCount = jsonObj[upper];
-					
-					const latest = uCount[uCount.length-1];
-					const yest = uCount[uCount.length-2];
-					
-					const cdiff = latest.confirmed - yest.confirmed
-					const ddiff = latest.deaths - yest.deaths
-					const rdiff = latest.recovered - yest.recovered
+  return {
+    full: countryData,
+    today: countryData[countryData.length - 1],
+    yesterday: countryData[countryData.length - 2]
+  }
+}
 
-					return ( "Currently in "+upper+" there are **"+latest.confirmed+"** *"+"(+"+cdiff+")*"+" cases of COVID19, with **"+latest.deaths+"** *(+"+ddiff+")*"+" deaths, and **"+latest.recovered+"** *(+"+rdiff+")*"+" recoveries as of **"+latest.date+"**." )	
-					
-				} else {
-					return "Country data not found in JHU database."
-				}
-			}
-		}
-    ]
+const baseConfig = {
+  command: "covid19",
+};
+
+export default function (bastion, opt = {}) {
+  const config = deepmerge(baseConfig, opt);
+
+  return [
+    {
+      // Command to start it
+      command: config.command,
+      options: bastion.parsers.args(["country"]),
+
+      // Core of the command
+      resolve: async function (context, country) {
+        // Fetch data
+        const countryString = capitalizeWords(country) || 'US'
+        const data = await getCountryStats(countryString)
+
+        // If no data return
+        if (!data) return `Country '${countryString}' data not found in JHU database.`;
+
+        // get diffs
+        const cdiff = data.today.confirmed - data.yesterday.confirmed;
+        const ddiff = data.today.deaths - data.yesterday.deaths;
+        const rdiff = data.today.recovered - data.yesterday.recovered;
+
+        return (
+          `Currently in ${countryString} there are **${data.today.confirmed}** *(+${cdiff})* cases of COVID19, ` +
+          `with **${data.today.deaths}** *(+${ddiff})* deaths, ` +
+          `and **${data.today.recovered}** *(+${rdiff})* recoveries as of ${data.today.date}`
+        );
+      },
+    },
+  ];
 }
