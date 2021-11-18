453b34c2dd1dbf10d100418b087e50e49cbabf5d
Seb <4062679+hellos3b@users.noreply.github.com>
2021-11-18 14:05:11 -0800

    Fix missing client references

--- backend/src/app/index.ts
+++ backend/src/app/index.ts
@@ -3,5 +3,3 @@ export * as MongoDb from './MongoDb';
 export * as DiscordClient from './DiscordClient';
 export * as HapiServer from './HapiServer';
 export * as Settings from './Settings';
-
-export { Instance } from './DiscordClient';
