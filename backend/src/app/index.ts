0da540a8bcd8ef1224d495d52abc46d59c758aa2
Seb <4062679+hellos3b@users.noreply.github.com>
2022-01-13 16:32:42 -0800

    internal: Moving types around

--- backend/src/app/index.ts
+++ backend/src/app/index.ts
@@ -1,5 +1,4 @@
 export * as env from './env';
 export * as MongoDb from './MongoDb';
 export * as DiscordClient from './DiscordClient';
-export * as HapiServer from './HapiServer';
 export * as Settings from './settings';
