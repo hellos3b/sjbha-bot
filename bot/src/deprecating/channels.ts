d56daca7c833f2ce563afab1395cba81ab88173a
Seb <4062679+hellos3b@users.noreply.github.com>
2023-03-17 17:48:25 -0700

    fix channel ids in env

--- bot/src/deprecating/channels.ts
+++ bot/src/deprecating/channels.ts
@@ -1,28 +1,10 @@
 import { env } from "../environment";
 
-const production = {
-   strava:            "450913008323919872",
-   shitpost:          "506911331257942027",
-   bot_admin:         "430517752546197509",
-   meetups_directory: "950985725334138880",
-   meetups:           "358442118928400384",
-   showdown:          "911438085781327913"
+export const channels = {
+   strava: env.CHANNEL_STRAVA,
+   shitpost: env.CHANNEL_SHITPOST,
+   bot_admin: env.CHANNEL_BOT_ADMIN,
+   meetups_directory: env.CHANNEL_MEETUPS_DIR,
+   meetups: env.CHANNEL_MEETUPS,
+   showdown: env.CHANNEL_THROWDOWN
 };
-
-type Channels = typeof production;
-
-const devServer = {
-   bot1: "861815778281259008",
-   bot2: "861816839197950002"
-};
-
-const development: Channels = {
-   strava:            devServer.bot1,
-   shitpost:          devServer.bot1,
-   bot_admin:         devServer.bot1,
-   meetups:           devServer.bot1,
-   meetups_directory: "876983154924204043",
-   showdown:          devServer.bot1
-};
-
-export const channels = env.NODE_ENV === "PRODUCTION" ? production : development;
