0e266c6b7d16570f7e7893f5dc849a734d5febf6
Seb <hellos3b@users.noreply.github.com>
2024-02-19 09:55:25 -0800

    fit: add hot streak to monday post

--- packages/fitness/src/MondayRecap.js
+++ packages/fitness/src/MondayRecap.js
@@ -1,4 +1,4 @@
-import { startOfWeek, subDays } from "date-fns";
+import { startOfWeek, subDays, subWeeks } from "date-fns";
 import { EmbedBuilder } from "discord.js";
 import schedule from "node-schedule";
 import { loggedWorkoutCollection } from "./LoggedWorkout";
@@ -6,6 +6,51 @@ import { effortScore } from "./EffortScore";
 
 const unique = (arr) => Array.from(new Set(arr));
 const notNull = (x) => !!x;
+const add = (a, b) => a + b;
+const average = (nums) =>
+   nums.length === 0 ? 0 : nums.reduce(add, 0) / nums.length;
+
+// Returns how much a user gets per week on average.
+// returns -1 if not enough data
+const expByWeek = (workouts) =>
+   Object.values(
+      workouts.reduce((dict, workout) => {
+         const inserted = new Date(workout.insertedDate);
+         const monday = startOfWeek(inserted, {
+            weekStartsOn: 1,
+         }).toISOString();
+
+         dict[monday] = (dict[monday] ?? 0) + workout.exp;
+         return dict;
+      }, {}),
+   );
+
+const Streak = {
+   NOOB: 0,
+   CONSISTENT: 1,
+   HOT: 2,
+   COLD: 3,
+};
+
+const streakToString = (streak) =>
+   ({
+      [Streak.COLD]: "❄️",
+      [Streak.CONSISTENT]: "",
+      [Streak.HOT]: "🔥",
+      [Streak.NOOB]: "👋",
+   })[streak];
+
+const getStreak = (weeklyExp, exp) => {
+   if (!weeklyExp.length) return Streak.NOOB;
+   if (weeklyExp.length < 3) return Streak.CONSISTENT;
+
+   const avg = average(weeklyExp);
+   if (exp < avg * 0.8) return Streak.COLD;
+   if (exp > avg * 1.2) return Streak.HOT;
+   return Streak.CONSISTENT;
+};
+
+// const
 
 /**
  * @param {import("discord.js").Client} discord
@@ -16,14 +61,22 @@ export const createRecap = (discord, db) => async () => {
    const thisMonday = startOfWeek(new Date(), { weekStartsOn: 1 });
    const lastMonday = subDays(thisMonday, 7);
 
-   const workoutsThisWeek = await workouts
+   const recentWorkouts = await workouts
       .find({
          insertedDate: {
-            $gt: lastMonday.toISOString(),
+            $gt: subWeeks(thisMonday, 7).toISOString(),
             $lt: thisMonday.toISOString(),
          },
       })
       .toArray();
+
+   const workoutsThisWeek = recentWorkouts.filter(
+      (x) => x.insertedDate >= lastMonday.toISOString(),
+   );
+
+   const workoutsBeforeThisWeek = recentWorkouts.filter(
+      (x) => x.insertedDate < lastMonday.toISOString(),
+   );
 
    const discordIds = unique(workoutsThisWeek.map((x) => x.discordId));
 
@@ -38,22 +91,34 @@ export const createRecap = (discord, db) => async () => {
          name: "Monday Moist Recap",
       },
       description:
-         "Here's everyone's total 💦 score from the last week!\n\n" +
+         "Here's everyone's total score from the previous week!\n\n" +
+         "💦💦💦\n\n" +
          members
             .filter((m) => workoutsThisWeek.some((x) => x.discordId === m.id))
             .map((member) => {
-               const exp = workoutsThisWeek
-                  .filter((x) => x.discordId === member.id)
-                  .reduce((sum, a) => sum + a.exp, 0);
+               const workouts = workoutsThisWeek.filter(
+                  (x) => x.discordId === member.id,
+               );
+
+               const exp = workouts.reduce((sum, a) => sum + a.exp, 0);
+               const prevWeekExps = expByWeek(
+                  workoutsBeforeThisWeek.filter(
+                     (x) => x.discordId === member.id,
+                  ),
+               );
 
                return {
                   username: member.nickname ?? member.user.username,
+                  count: workouts.length,
                   score: effortScore(exp),
+                  streak: getStreak(prevWeekExps, exp),
                };
             })
             .filter(notNull)
             .sort((a, b) => (a.score > b.score ? -1 : 1))
-            .map((x) => `🔹 **${x.username}** ${x.score.toFixed(1)}`)
+            .map((x) => {
+               return `**${x.username}**🔹${x.score.toFixed(1)} (${x.count}) ${streakToString(x.streak)}`;
+            })
             .join("\n"),
    });
 };
